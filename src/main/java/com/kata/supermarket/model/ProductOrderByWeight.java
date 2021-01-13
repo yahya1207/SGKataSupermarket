47df856c904d0e9b501204a5fe190c0d27e3a506
yahya1207 <hajji.yahya@gmail.com>
2021-01-13 03:13:46 +0100

    implements calculate product order by weight without offer to success the test

--- src/main/java/com/kata/supermarket/model/ProductOrderByWeight.java
+++ src/main/java/com/kata/supermarket/model/ProductOrderByWeight.java
@@ -4,13 +4,18 @@ import com.kata.supermarket.type.WeightUnity;
 
 public class ProductOrderByWeight implements IProductOrder {
 
+    private Product product;
+    private int orderAmount;
+    private WeightUnity orderUnity;
 
     public ProductOrderByWeight(Product product, int orderAmount, WeightUnity orderUnity) {
+        this.product = product;
+        this.orderAmount = orderAmount;
+        this.orderUnity = orderUnity;
     }
 
     @Override
     public double calculateProductOrderPrice() {
-        return 0;
+        return product.getInitialPrice() * orderAmount;
     }
-
 }
