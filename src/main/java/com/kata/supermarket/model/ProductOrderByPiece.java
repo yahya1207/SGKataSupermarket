ecfc3af51c003997366553b7060e80705bf7fc68
yahya1207 <hajji.yahya@gmail.com>
2021-01-13 02:14:47 +0100

    implement calculate Product Order to success the test

--- src/main/java/com/kata/supermarket/model/ProductOrderByPiece.java
+++ src/main/java/com/kata/supermarket/model/ProductOrderByPiece.java
@@ -2,11 +2,16 @@ package com.kata.supermarket.model;
 
 public class ProductOrderByPiece implements IProductOrder {
 
+    private Product product;
+    private int orderAmount;
+
     public ProductOrderByPiece(Product product, int orderAmount) {
+        this.product = product;
+        this.orderAmount = orderAmount;
     }
 
     @Override
     public double calculateProductOrderPrice() {
-        return 0;
+        return product.getInitialPrice() * orderAmount;
     }
 }
