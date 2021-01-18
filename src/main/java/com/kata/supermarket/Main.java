01fb846df4dc5b518b3eb4f64891f4dfe3e92c70
yahya1207 <hajji.yahya@gmail.com>
2021-01-18 05:31:52 +0100

    Add example of placing an order in the main class

--- src/main/java/com/kata/supermarket/Main.java
+++ src/main/java/com/kata/supermarket/Main.java
@@ -1,8 +1,37 @@
 package com.kata.supermarket;
 
+import com.kata.supermarket.model.Offer;
+import com.kata.supermarket.model.Product;
+import com.kata.supermarket.repository.IOfferRepository;
+import com.kata.supermarket.repository.OfferRepository;
+import com.kata.supermarket.service.OrderCalculator;
+
+import java.math.BigDecimal;
+
 public class Main {
 
-    public static void main(String[] args){
-        // see test classes for how to place an order
+    public static void main(String[] args) {
+
+        //Available products
+        Product tomatoes = new Product("tomatoes", new BigDecimal(1.99));
+        Product milk = new Product("milk Candia", new BigDecimal(1.2));
+
+        //Available offers
+        Offer tomatoesOffer = new Offer(BigDecimal.valueOf(3), BigDecimal.valueOf(5.5));
+        Offer milkOffer = new Offer(BigDecimal.valueOf(6), BigDecimal.valueOf(5));
+
+        IOfferRepository offerRepository = OfferRepository.getInstance();
+        offerRepository.addOffer(tomatoes, tomatoesOffer);
+        offerRepository.addOffer(milk, milkOffer);
+
+        //place an order
+        OrderCalculator clientOrder = new OrderCalculator();
+
+        BigDecimal totalPrice = clientOrder.addProductOrder(tomatoes, BigDecimal.valueOf(5))
+                .addProductOrder(milk, BigDecimal.valueOf(11))
+                .getTotalPrice();
+
+
+        System.out.println("Le total = " + totalPrice);
     }
 }
